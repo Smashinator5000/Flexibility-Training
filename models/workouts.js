36e58fe07ed548e441a9ef015708673bb730be19
Ashley Bartholomew <ashley.52389@gmail.com>
2021-02-23 14:58:06 -0700

    done product

---
+++ models/workouts.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+  {
+  exercises: [
+  {
+  type: {
+    type: String,
+    trim: true,
+    required: "What type of exercise are you doing?"
+  },
+
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter an exercise name"
+  },
+
+  weight: {
+    type: Number
+  },
+
+  sets: {
+    type: Number
+  },
+
+  reps: {
+    type: Number
+  },
+
+  duration: {
+    type: Number,
+    required: "How many minutes did you work out?",
+    default: 0
+  },
+
+  distance: {
+    type: Number,
+  }
+
+  }],
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+},
+{
+  toJSON: {
+    virtuals: true
+  }
+}
+
+);
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce(
+    (total, exercise) => {
+    return total + exercise.duration
+  }, 0)
+})
+const Workouts = mongoose.model("Workouts", WorkoutSchema);
+
+module.exports = Workouts;
---
+++ routes/api.js
@@ -0,0 +1,59 @@
+const app = require("express").Router();
+const Fitness = require("../models/workouts.js");
+
+app.post("/api/workouts", (req, res) => {
+    Fitness.create({})
+    .then((fitnessDB) => {
+        res.json(fitnessDB)
+    })
+    .catch((err) => {
+        res.json(err)
+        console.log(err)
+    })
+});
+
+app.put("/api/workouts/:id", ({ body, params}, res) => {
+    Fitness.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } } ,
+    )
+    .then(fitnessDB => {
+        res.json(fitnessDB);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+app.get("/api/workouts", (req, res) => {
+    Fitness.find()
+    .then((fitnessDB) => {
+        console.log(fitnessDB)
+        res.json(fitnessDB)
+    })
+});
+
+app.get("/api/workouts/range", (req, res) => {
+        Fitness.find()
+        .then(fitnessDB => {
+            res.json(fitnessDB);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    }); 
+
+
+app.delete("/api/workouts", ({ body }, res) => {
+  Fitness.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+    
+   
+
+module.exports = app;
